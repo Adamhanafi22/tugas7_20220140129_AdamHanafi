5e18c4c8ab0e98c0b11f4023b0e3ff78d59233e0
Adamhanafi22 <adamhanafi1922@gmail.com>
2024-04-05 20:04:59 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video() {
+    alert("Ini Video So Far Away");
+})
+
+audio.addEventListener("click", function audio() {
+    alert("Ini Audio So Far Away");
+})
+
+gambar.addEventListener("click", function gambar() {
+    alert("Ini Gambar So Far Away");
+})
